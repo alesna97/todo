9d084e205461154d6cbf94d864653ac52dd10531
anjas lesmana <alesna97@gmail.com>
2023-01-10 21:09:41 +0700

    feat: create todo component

--- src/components/TodoCard.js
+++ src/components/TodoCard.js
@@ -1,12 +1,37 @@
-import { Card } from "@mui/material";
+import { Delete, Edit } from "@mui/icons-material";
+import { Button, Card, IconButton, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 
-const TodoCard = ({ text }) => {
+const TodoCard = ({ text, completed }) => {
+  const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
   return (
-    <Card style={{ width: "100%" }}>
-      <Box padding={2} width="100%">
-        {text}
+    <Card
+      style={{
+        width: "100%",
+        opacity: completed ? 0.5 : 1,
+        borderLeft: `solid 4px ${
+          colors[[Math.floor(Math.random() * colors.length)]]
+        }`,
+      }}
+    >
+      <Box padding={2} width="100%" display="flex" alignItems="center">
+        <Box flex={1} ml={1}>
+          <Typography variant="body1">{text}</Typography>
+        </Box>
+        <Box display="flex" alignItems="center">
+          <IconButton size="small" disabled={completed}>
+            <Edit fontSize="small" />
+          </IconButton>
+          <IconButton size="small" disabled={completed}>
+            <Delete fontSize="small" />
+          </IconButton>
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="end">
+        <Button variant="text" disabled={completed}>
+          Mark as complete
+        </Button>
       </Box>
     </Card>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,9 +5,15 @@ import React from "react";
 
 const TodoList = ({ data = [], loading = false }) => {
   return (
-    <Box display="flex" flexDirection="column" gap={4} alignItems="center">
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      padding={2}
+      gap={2}
+    >
       {data.map((item) => (
-        <TodoCard text={item.todo} key={item.id} />
+        <TodoCard text={item.todo} key={item.id} completed={item.completed} />
       ))}
       {loading && <CircularProgress size={24} />}
     </Box>
